fc4368a62073c902c3382bcbd7483b4e832e46e3
Ibrahim Al-Ali <ibrahim.alali94@yahoo.ro>
2023-11-09 13:33:27 +0000

    forum

--- handlers/filerPostHandler.go
+++ handlers/filerPostHandler.go
@@ -9,8 +9,9 @@ import (
 )
 
 func FilterPosts(w http.ResponseWriter, r *http.Request, db *sql.DB) {
-	userID, ok := GetAuthenticatedUserID(r)
-	if !ok {
+	// Check if the user is authenticated
+	userID, isAuthenticated := GetAuthenticatedUserID(r)
+	if !isAuthenticated {
 		http.Redirect(w, r, "/login", http.StatusSeeOther)
 		return
 	}
@@ -21,27 +22,27 @@ func FilterPosts(w http.ResponseWriter, r *http.Request, db *sql.DB) {
 	liked := r.FormValue("liked")
 
 	// Initialize an empty slice to hold filtered posts.
-	var filteredPosts []database.Post
+	var posts []database.Post
 
 	// 2. Based on the criteria, call the corresponding functions to retrieve the filtered posts.
 	switch {
 	case category != "":
 		// Filter by category
-		filteredPosts, _ = database.GetPostsByCategory(db, category)
+		posts, _ = database.GetPostsByCategory(db, category)
 	case created == "true":
 		// Filter created posts by the authenticated user
-		filteredPosts, _ = database.GetOwnedPosts(db, userID)
+		posts, _ = database.GetOwnedPosts(db, userID)
 	case liked == "true":
 		// Filter liked posts by the authenticated user
-		filteredPosts, _ = database.GetLikedPosts(db, userID, true)
+		posts, _ = database.GetLikedPosts(db, userID, true)
 	default:
 		// No criteria selected, show all posts
-		filteredPosts, _ = database.GetPosts(db)
+		posts, _ = database.GetPosts(db)
 	}
 
 	// 3. For each post, retrieve like/dislike counts and usernames for comments.
-	for i, post := range filteredPosts {
-		comments, err := database.GetCommentsForPost(db, post.ID)
+	for i := range posts {
+		comments, err := database.GetCommentsForPost(db, posts[i].ID)
 		if err != nil {
 			http.Error(w, "Error fetching comments", http.StatusInternalServerError)
 			log.Println(err)
@@ -49,28 +50,31 @@ func FilterPosts(w http.ResponseWriter, r *http.Request, db *sql.DB) {
 		}
 
 		// Fetch like/dislike counts and usernames for comments
-		for j, comment := range comments {
-			likeCount, dislikeCount, err := database.GetCommentLikesCount(db, comment.ID)
+		for j := range comments {
+			likeCount, dislikeCount, err := database.GetCommentLikesCount(db, comments[j].ID)
 			if err != nil {
 				http.Error(w, "Error fetching comment likes/dislikes", http.StatusInternalServerError)
 				log.Println(err)
 				return
 			}
 
-			// username, err := database.GetUserByID(db, comment.UserID)
+			comments[j].LikeCount = likeCount
+			comments[j].DislikeCount = dislikeCount
+
+			// Assuming you have a function to get the username by user ID
+			// username, err := database.GetUserByID(db, comments[j].UserID)
 			// if err != nil {
 			// 	http.Error(w, "Error fetching username", http.StatusInternalServerError)
 			// 	log.Println(err)
 			// 	return
 			// }
 
-			comments[j].LikeCount = likeCount
-			comments[j].DislikeCount = dislikeCount
+			// Assign the username to the comment
 			// comments[j].Username = username
 		}
 
 		// Assign comments to the post
-		filteredPosts[i].Comments = comments
+		posts[i].Comments = comments
 	}
 
 	// 4. Render the filtered posts to the page.
@@ -83,14 +87,20 @@ func FilterPosts(w http.ResponseWriter, r *http.Request, db *sql.DB) {
 	}{
 		IsAuthenticated: userData.IsAuthenticated,
 		Username:        userData.Username,
-		Posts:           filteredPosts,
+		Posts:           posts,
 	}
 
 	tmpl, err := template.ParseFiles("templates/index.html")
 	if err != nil {
 		http.Error(w, "Error Parsing index.html", http.StatusInternalServerError)
+		log.Println(err)
 		return
 	}
 
-	tmpl.Execute(w, data)
+	err = tmpl.Execute(w, data)
+	if err != nil {
+		http.Error(w, "Error rendering the template", http.StatusInternalServerError)
+		log.Println(err)
+		return
+	}
 }
